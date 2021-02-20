8e1c498d5f8d78ae27d3de60065724fd164267a8
soumya <soumyaranjanpalai983@gmail.com>
2021-02-20 20:24:52 +0530

    modify test class

--- DemoProj1/src/com/pk/test/TestC.java
+++ DemoProj1/src/com/pk/test/TestC.java
@@ -1,5 +1,11 @@
 package com.pk.test;
 
+import com.pk.beans.Utility;
+
 public class TestC {
 
+	public static void main(String[] args) {
+		Utility u = new Utility();
+		System.out.println(u.getDate());
+	}
 }
