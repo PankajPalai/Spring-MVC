304246cc84600aad364a064a7cb4fb4d63936616
soumya <soumyaranjanpalai983@gmail.com>
2021-02-20 20:50:54 +0530

    added new pkg by dev 1

---
+++ DemoProj1/src/com/pk/comp/TestD.java
@@ -0,0 +1,12 @@
+package com.pk.comp;
+
+import com.pk.beans.Utility;
+
+public class TestD {
+
+	public static void main(String[] args) {
+		Utility u = new Utility();
+		System.out.println("====================");
+		System.out.println(u.getDate());
+	}
+}
